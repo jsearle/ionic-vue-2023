13a7c860fe4c338beed79a32f600bca661929249
Juan Searle (Casa) <jsearle@gmail.com>
2023-01-26 12:16:02 +0100

    MiComponenteDemo creado

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <MiComponenteDemo />
+    <MiComponenteDemo />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import MiComponenteDemo from './components/MiComponenteDemo.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    'MiComponenteDemo' : MiComponenteDemo
   }
 });
 </script>
---
+++ demo/src/components/MiComponenteDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Bienvenid@s a VUE!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'MiComponenteDemo',
+  components: {
+  }
+})
+
+</script>
+
+<style>
+h1{
+  color:red;
+}
+</style>
