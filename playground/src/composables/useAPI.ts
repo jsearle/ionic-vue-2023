88ef7fb2e789e31af8bb23594202e07af88d962d
Juan Searle (Casa) <jsearle@gmail.com>
2023-01-31 11:12:46 +0100

    Api integration y comentarios

--- playground/src/composables/useAPI.ts
+++ playground/src/composables/useAPI.ts
@@ -1,15 +1,23 @@
 
-
+// Definición de un composable (función exportada en default)
 export default function useAPI(){
+  // constantes que no van a ser reactivas
   const API_USERS_URL = 'https://reqres.in/api/users'
   const API_USER_LOGIN = 'https://reqres.in/api/login'
 
+  // función asíncrona para obtener los datos de usuarios
   const loadUserList = async () => {
+    // carga de los datos con await para esperar a la promesa
+    // result.json() sirve para desenvolver la petición http en un formato JSON
     const response = await fetch(API_USERS_URL).then(result => result.json())
+    // extraemos la propiedad data, que contiene un array de usuarios
     const list = response.data
+    // devolvemos la lista de usuarios
     return list
   }
   
+
+  // respuesta del composable: funciones y variables reactivas
   return {
     loadUserList
   }
--- playground/src/views/UsersListPage.vue
+++ playground/src/views/UsersListPage.vue
@@ -7,20 +7,24 @@
     </ion-header>
     <ion-content>
       <ion-list>
-        <ion-item>
+        <!-- las variables de estado en la plantilla se acceden directamente (sin "this.")  -->
+        <ion-item v-for="user in users" :key="user.id">
+          <ion-avatar slot="start">
+            <ion-img :src="user.avatar"></ion-img>
+          </ion-avatar>
           <ion-label>
-            <h2>Usuario</h2>
-            <p>Email del usuario</p>
+            <h2>{{ user.first_name }} {{ user.last_name }}</h2>
+            <p>{{ user.email }}</p>
           </ion-label>
         </ion-item>
       </ion-list>
-      <p>{{users}}</p>
-      <ion-button @click="loadUsers()">Cargar usuarios</ion-button>
+      <ion-button expand="block" size="large" @click="loadUsers()">Cargar usuarios</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+// importamos los componentes de ionic
 import {
   IonContent,
   IonHeader,
@@ -30,13 +34,22 @@ import {
   IonList,
   IonLabel,
   IonItem,
-  IonButton
+  IonButton,
+  IonAvatar,
+  IonImg
 } from "@ionic/vue";
+
+// importamos la función para definir un componente en TS
 import { defineComponent } from "vue";
+
+// importamos nuestra composable function
 import useAPI from "../composables/useAPI";
 
+// definimos el componente
 export default defineComponent({
+  // nombre del componente para las Dev Tools
   name: "UsersListPage",
+  // registramos todos los componentes que se van a usar en la plantilla
   components: {
     IonContent,
     IonHeader,
@@ -46,21 +59,27 @@ export default defineComponent({
     IonList,
     IonLabel,
     IonItem,
-    IonButton
+    IonButton,
+    IonAvatar,
+    IonImg
   },
+  // definimos las variables de estado (reactivas) de este componente
   data(){
     return {
-      users: []
+      users: [] as any[]
     }
   },
+  // definimos los métodos que serán llamados desde la plantilla
   methods:{
     async loadUsers(){
+      // siempre usamos this dentro de los métodos para acceder a varaibles o a otros métodos del componente
       this.users = await this.api.loadUserList()
     }
   },
+  // montamos las instancias de las composable functions
   setup() {
     const api = useAPI()
-    return {api};
+    return { api };
   },
 });
 </script>
