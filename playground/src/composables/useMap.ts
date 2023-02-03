92115a2ecc16f8f98aece5ab0aa83b2adbc02929
Juan Searle (Casa) <jsearle@gmail.com>
2023-02-03 14:12:46 +0100

    Estilos del mapa!

--- playground/src/composables/useMap.ts
+++ playground/src/composables/useMap.ts
@@ -65,7 +65,7 @@ export default function useMap() {
     const opts:any = {
       center: center,
       zoom: zoom,
-      //styles: mapStyles,
+      styles: mapStyles,
       maxZoom: 20,
       minZoom: 0,
       mapTypeId: 'terrain',
@@ -83,7 +83,7 @@ export default function useMap() {
     const loader = new Loader({
       apiKey: 'AIzaSyDKtd8L97X6mEGTEIqyuZzj3m1mBNDAlgk',
       version: '3.31',
-      libraries: ['places','geometry']
+      libraries: ['places','geometry','drawing']
     });
     google = await loader.load()
     isLoaded.value = true;
@@ -104,3 +104,142 @@ export default function useMap() {
   }
 
 }
+
+
+const mapStyles = [
+  {
+      "featureType": "all",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#202c3e"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "gamma": 0.01
+          },
+          {
+              "lightness": 20
+          },
+          {
+              "weight": "1.39"
+          },
+          {
+              "color": "#ffffff"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+          {
+              "weight": "0.96"
+          },
+          {
+              "saturation": "9"
+          },
+          {
+              "visibility": "on"
+          },
+          {
+              "color": "#000000"
+          }
+      ]
+  },
+  {
+      "featureType": "all",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 30
+          },
+          {
+              "saturation": "9"
+          },
+          {
+              "color": "#29446b"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "saturation": 20
+          }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 20
+          },
+          {
+              "saturation": -20
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 10
+          },
+          {
+              "saturation": -30
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#193a55"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "saturation": 25
+          },
+          {
+              "lightness": 25
+          },
+          {
+              "weight": "0.01"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [
+          {
+              "lightness": -20
+          }
+      ]
+  }
+]
